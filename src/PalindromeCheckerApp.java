12fa73eafcc2d9543a5fd9ff0c3e8324fa9ceae2
akhil <akhilsama1210@gmail.com>
2026-03-05 09:04:39 +0530

    UC3: User input palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String word = "madam";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a word:");
+        String word = sc.nextLine();
+
         String reverse = "";
 
         for(int i = word.length()-1; i >= 0; i--) {
